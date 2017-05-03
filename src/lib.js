d06d797d661152f520725df50d09a97701c42a95
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-03 15:28:12 +0200

    Fix next handling

--- src/lib.js
+++ src/lib.js
@@ -3,13 +3,7 @@
 module.exports = handler => async (req, res, next) => {
   try {
     await handler(req, res, next);
-    if (next) {
-      next();
-    }
   } catch (e) {
-    if (next) {
-      next(e);
-    }
     if (!res.headersSent) {
       const status = e.status || 500;
       if (e.toString) {
